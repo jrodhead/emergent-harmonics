d6c3bcd86c1dba55ecb1a63d053d8233421688bf
jrodhead <13279619+jrodhead@users.noreply.github.com>
2024-01-15 10:43:14 -0600

    ratio formatting

--- js/systemCalculators/ratioGenerators.js
+++ js/systemCalculators/ratioGenerators.js
@@ -1,23 +1,23 @@
 // todo: turn these into functions that can handle any number of notes in a diapason
 
 export const majorScaleRatios = [
-  1, // Tonic
-  9 / 8, // Tonic to Supertonic (2nd degree)
-  5 / 4, // Tonic to Mediant (3rd degree)
-  4 / 3, // Tonic to Subdominant (4th degree)
-  3 / 2, // Tonic to Dominant (5th degree)
-  5 / 3, // Tonic to Submediant (6th degree)
+   1,     // Tonic
+   9 / 8, // Tonic to Supertonic (2nd degree)
+   5 / 4, // Tonic to Mediant (3rd degree)
+   4 / 3, // Tonic to Subdominant (4th degree)
+   3 / 2, // Tonic to Dominant (5th degree)
+   5 / 3, // Tonic to Submediant (6th degree)
   15 / 8 // Tonic to Leading Tone (7th degree)
 ];
 
 export const minorScaleRatios = [
-  1,     // root
-  16/15, // minorSecond
-  6/5,   // minorThird
-  4/3,   // perfectFourth
-  45/32, // diminishedFifth
-  8/5,   // minorSixth
-  9/5    //minorSeventh
+   1,      // root
+  16 / 15, // minorSecond
+   6 / 5,  // minorThird
+   4 / 3,  // perfectFourth
+  45 / 32, // diminishedFifth
+   8 / 5,  // minorSixth
+   9 / 5   // minorSeventh
 ];
 
 export const hd110067Ratios = [
