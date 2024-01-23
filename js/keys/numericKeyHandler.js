f4eaf0ff628c0bdeccc0c44b5fcd79d9d902aecb
jrodhead <13279619+jrodhead@users.noreply.github.com>
2024-01-23 15:19:17 -0600

    Refactor numeric key handling in numericKeyHandler.js

--- js/keys/numericKeyHandler.js
+++ js/keys/numericKeyHandler.js
@@ -1,18 +1,15 @@
 import { musicalSystemGlobal } from '../main.js';
 import { createAlphaKeyMap } from "./alphaKeyMap.js";
 
-/**
- * Handles the numeric key event.
- * @param {string} ev - The event type ('keydown' or 'keyup').
- * @param {number} rootIndex - The root index.
-*/
 let currentRootIndex = 0;
 export { currentRootIndex };
+
+const numericKeys = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 const handleNumericKey = (ev, rootIndex) => {
   if (ev === 'keydown') {
     console.log(`${rootIndex}On`);
-    if (rootIndex >= 0 && rootIndex < musicalSystemGlobal.length) {
+    if (isValidRootIndex(rootIndex)) {
       currentRootIndex = rootIndex;
       createAlphaKeyMap(musicalSystemGlobal);
     } else {
@@ -23,8 +20,21 @@ const handleNumericKey = (ev, rootIndex) => {
   }
 };
 
+const isValidRootIndex = (rootIndex) => {
+  return rootIndex >= 0 && rootIndex < musicalSystemGlobal.length;
+};
+
+const setActiveRoot = (keyIndex) => {
+  let activeRoot = document.getElementById(`root${keyIndex}`);
+  const activeElements = document.querySelectorAll('.active');
+  activeElements.forEach(element => {
+    element.classList.remove('active');
+  });
+
+  activeRoot.classList.add('active');
+};
+
 document.body.addEventListener('keydown', (ev) => {
-  const numericKeys = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
   const keyIndex = numericKeys.indexOf(ev.key);
 
   if (ev.repeat) return;
@@ -35,20 +45,12 @@ document.body.addEventListener('keydown', (ev) => {
 });
 
 document.body.addEventListener('keyup', (ev) => {
-  const numericKeys = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
   const keyIndex = numericKeys.indexOf(ev.key);
-  let activeRoot = document.getElementById(`root${keyIndex}`);
 
   if (ev.repeat) return;
 
   if (keyIndex !== -1) {
     handleNumericKey('keyup', keyIndex);
-
-    const activeElements = document.querySelectorAll('.active');
-    activeElements.forEach(element => {
-      element.classList.remove('active');
-    });
-
-    activeRoot.classList.add('active');
+    setActiveRoot(keyIndex);
   }
 });
