a4ae1425ddbc4af5b0f37f024ea316bde0788cc6
jrodhead <13279619+jrodhead@users.noreply.github.com>
2024-03-15 13:38:52 -0500

    Remove isValidDiapasons function

--- js/keys/isValidSystem.js
+++ js/keys/isValidSystem.js
@@ -6,12 +6,3 @@
 export function isValidSystem(system) {
   return Array.isArray(system) && system.length > 0;
 }
-
-/**
- * Checks if the provided diapasons are valid.
- * @param {Array} diapasons - The diapasons to be checked.
- * @returns {boolean} - True if the diapasons are valid, false otherwise.
- */
-export function isValidDiapasons(diapasons) {
-  return diapasons && Array.isArray(diapasons) && diapasons.length > 0;
-}
